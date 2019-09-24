2ad7b4ea37569926ff1a6d4e7095e44e2a9be951
Abel <abelmasila@gmail.com>
2019-09-24 17:32:57 +0300

    added update track mutation

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -34,5 +34,29 @@ class CreateTrack(graphene.Mutation):
         return CreateTrack(track=track)
 
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType);
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, track_id, title, description, url):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Log in to add a track")
+        track = Track.objects.get(id=track_id)
+        if (track.posted_by != user):
+            raise Exception("Not permitted to update this track")
+        track.title=title
+        track.description=description
+        track.url=url
+        track.save()
+        return  UpdateTrack(track=track)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track=UpdateTrack.Field()
