77d5a86847707780dc94bfb93937d22b865f61a6
Abel <abelmasila@gmail.com>
2019-09-24 16:29:42 +0300

    added mutation to create a track

--- app/app/schema.py
+++ app/app/schema.py
@@ -6,5 +6,8 @@ class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
 
+
+schema = graphene.Schema(query=Query,mutation=Mutation)
